0905d48ea1fa8cdd5162aa80af8d690c5cd82c8e
Viktor Kulia <65856881+ViktorsK1@users.noreply.github.com>
2020-09-30 00:44:16 +0300

    Add files via upload

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Виктор Куля on 02.08.2020.
+//  Copyright © 2020 Виктор Куля. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
